d5edd26e2e80976c0cb1520b2fc0535eae42fc5b
kamecha <masakikameyama81@gmail.com>
2024-02-23 20:24:05 +0900

    とりあえずparamsをそのまま流すようにした

--- denops/@ddu-sources/custom_params.ts
+++ denops/@ddu-sources/custom_params.ts
@@ -1,13 +1,22 @@
 import { GatherArguments } from "https://deno.land/x/ddu_vim@v3.10.2/base/source.ts";
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
 
-type Params = Record<never, never>;
+type Params = {
+  items: Item<unknown>[];
+};
 
 export class Source extends BaseSource<Params> {
-  gather({}: GatherArguments<Params>): ReadableStream<Item<unknown>[]> {
-    throw new Error("Method not implemented.");
+  gather(args: GatherArguments<Params>): ReadableStream<Item<unknown>[]> {
+    return new ReadableStream({
+      start(controller) {
+        controller.enqueue(args.sourceParams.items);
+        controller.close();
+      },
+    });
   }
   params(): Params {
-    throw new Error("Method not implemented.");
+    return {
+      items: [],
+    };
   }
 }
