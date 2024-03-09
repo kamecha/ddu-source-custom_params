b6a789503e437c2aa50848f9ff8954b35c6c3e2e
kamecha <masakikameyama81@gmail.com>
2024-03-09 12:13:12 +0900

    add gather callback feature

--- denops/@ddu-sources/custom_params.ts
+++ denops/@ddu-sources/custom_params.ts
@@ -3,13 +3,28 @@ import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
 
 type Params = {
   items: Item<unknown>[];
+  gatherCallbackId: string;
 };
 
 export class Source extends BaseSource<Params> {
   gather(args: GatherArguments<Params>): ReadableStream<Item<unknown>[]> {
     return new ReadableStream({
-      start(controller) {
-        controller.enqueue(args.sourceParams.items);
+      async start(controller) {
+        let items: Item<unknown>[] = args.sourceParams.items;
+        // denops#callback#callはエラーを投げることがあるのでtry-catchする
+        if (args.sourceParams.gatherCallbackId !== "") {
+          try {
+            const gatheredItems = await args.denops.call(
+              "denops#callback#call",
+              args.sourceParams.gatherCallbackId,
+              args,
+            );
+            items = items.concat(gatheredItems as Item<unknown>[]);
+          } catch (e) {
+            console.error(e);
+          }
+        }
+        controller.enqueue(items);
         controller.close();
       },
     });
@@ -17,6 +32,7 @@ export class Source extends BaseSource<Params> {
   params(): Params {
     return {
       items: [],
+      gatherCallbackId: "",
     };
   }
 }
